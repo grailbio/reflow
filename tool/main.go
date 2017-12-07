7f9bda0fd0608cf375e9ca199a4585746969bb86
marius a. eriksen <marius@grailbio.com>
2017-12-07 20:34:47 +0000

    reflow/tool: wrap the command's MakeConfig only after creating the underlying config
    Summary:
    This change changes the order in which a command's MakeConfig is
    applied, from first to last. This allows overrides to be in place
    before GRAIL's ec2cluster is configured. This way, a user can override
    aspects of the configuration used by the cluster (e.g., https certs).
    
    Reviewers: sbagaria
    
    Reviewed By: sbagaria
    
    Differential Revision: https://phabricator.grailbio.com/D8547
    
    fbshipit-source-id: f01a5bb

--- tool/main.go
+++ tool/main.go
@@ -208,17 +208,18 @@ func (c *Cmd) Main() {
 		}
 		c.Config.Keys()[k] = *v
 	}
-	if c.MakeConfig != nil {
-		// The whole business of wrapping configuration is getting
-		// ugly. We should rethink this configuration system a little.
-		c.Config = c.MakeConfig(c.Config)
-	}
 	var err error
 	c.Config, err = config.Make(c.Config)
 	if err != nil {
 		c.Fatal(err)
 	}
-
+	// Run MakeConfig last, so that they can be properly composed with
+	// underlying config overrides.
+	if c.MakeConfig != nil {
+		// The whole business of wrapping configuration is getting
+		// ugly. We should rethink this configuration system a little.
+		c.Config = c.MakeConfig(c.Config)
+	}
 	if c.httpFlag != "" {
 		go func() {
 			c.Fatal(http.ListenAndServe(c.httpFlag, nil))
