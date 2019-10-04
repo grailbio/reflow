e8f6d94d18889fd995117d1a546473661e9e452e
Swami Mahadevan <smahadevan@grailbio.com>
2019-10-03 19:54:21 -0700

    reflowbootstrap: Refactor grail and OSS bootstrap to share code
    Summary:
    Refactor the bootstrap server code and share most functionality
    between the grail and open source bootstrap binaries.
    
    Haven't updated the docker image for both Grail and OSS with this change,
    because for the most part this change is a no-op, but I have tested it.
    
    Test Plan:
    For grail, built an image with tag `reflowbootstrap_test` and verified
    that a simple reflow program `reflow/remote_test` runs correctly.
    
    For OSS, built my own docker image (`smahadevan/reflowbootstrap_test`)
    and tested on my personal AWS account.
    
    Reviewers: dnicolaou, jcharumilind, pgopal, marius
    
    Reviewed By: dnicolaou
    
    Differential Revision: https://phabricator.grailbio.com/D37354
    
    fbshipit-source-id: 3f23627

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -8,56 +8,129 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
-	"github.com/grailbio/reflow/bootstrap/common"
-
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/grailbio/infra"
+	_ "github.com/grailbio/infra/aws"
+	_ "github.com/grailbio/infra/ec2metadata"
+	infratls "github.com/grailbio/infra/tls"
 	"github.com/grailbio/reflow/blob"
 	"github.com/grailbio/reflow/blob/s3blob"
+	"github.com/grailbio/reflow/bootstrap/common"
+	infra2 "github.com/grailbio/reflow/infra"
 	"github.com/grailbio/reflow/log"
 	"github.com/grailbio/reflow/rest"
+	"gopkg.in/yaml.v2"
 )
 
-// A BootstrapServer exports a simple service which allows clients to execute any image or binary referenced
-// by an S3 path with a given set of arguments
-type BootstrapServer struct {
-	// AwsSession is the AWS session instance this server will use to make AWS (specifically S3) calls
-	AwsSession *session.Session
+var (
+	// DefaultSchema defines the default schema for the Bootstrap server.
+	DefaultSchema = infra.Schema{
+		infra2.AWSCreds: new(credentials.Credentials),
+		infra2.Log:      new(log.Logger),
+		infra2.Session:  new(session.Session),
+		infra2.SSHKey:   new(infra2.SshKey),
+		infra2.TLS:      new(infratls.Authority),
+	}
+	// DefaultSchemaKeys defines the default infra keys for the Bootstrap server.
+	DefaultSchemaKeys = infra.Keys{
+		infra2.AWSCreds: "awscreds",
+		infra2.Log:      "logger",
+		infra2.Session:  "awssession",
+		infra2.SSHKey:   "key",
+		infra2.TLS:      "tls,file=/tmp/ca.reflow",
+	}
+)
 
-	// ServerConfig is the tls config specified in the reflow config
-	ServerConfig *tls.Config
+// RunServer runs the bootstrap server.
+func RunServer(schema infra.Schema, schemaKeys infra.Keys, configFile, addr string, insecure bool) {
+	if configFile != "" {
+		b, err := ioutil.ReadFile(configFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		keys := make(infra.Keys)
+		if err := yaml.Unmarshal(b, keys); err != nil {
+			log.Fatal("config %v: %v", configFile, err)
+		}
+		for k, v := range keys {
+			schemaKeys[k] = v
+		}
+	}
+	var err error
+	config, err := schema.Make(schemaKeys)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Addr is the address on which to listen.
-	Addr string
+	var sess *session.Session
+	if err = config.Instance(&sess); err != nil {
+		log.Fatal(err)
+	}
+	var tlsa *infratls.Authority
+	if err = config.Instance(&tlsa); err != nil {
+		log.Fatal(err)
+	}
+	_, serverConfig, err := tlsa.HTTPS()
+	if err != nil {
+		log.Fatal(err)
+	}
+	serverConfig.ClientAuth = tls.RequireAndVerifyClientCert
 
-	// Insecure listens on HTTP, not HTTPS.
-	Insecure bool
+	serv := server{
+		addr:     addr,
+		insecure: insecure,
+		sess:     sess,
+		config:   serverConfig,
+	}
+
+	log.Fatal(serv.listenAndServe())
 }
 
-// ListenAndServe serves the bootstrap server on the configured address.
-func (s *BootstrapServer) ListenAndServe() error {
-	bl := &blob.Mux{"s3": s3blob.New(s.AwsSession)}
+// A server exports a simple service which allows clients to execute any image or binary referenced
+// by an S3 path with a given set of arguments
+type server struct {
+	// sess is the AWS session instance this server will use to make AWS (specifically S3) calls
+	sess *session.Session
 
-	statNode := newStatusNode()
-	http.Handle("/v1/status", rest.DoFuncHandler(statNode, nil))
+	// config is the tls config specified in the reflow config
+	config *tls.Config
 
-	const expiry = 10 * time.Minute
-	ticker := time.NewTicker(expiry)
+	// addr is the address on which to listen.
+	addr string
 
-	go func() {
-		<-ticker.C
-		log.Fatalf("no bootstrap image installed after %s; shutting down", expiry)
-	}()
+	// insecure listens on HTTP, not HTTPS.
+	insecure bool
+}
+
+// listenAndServe serves the bootstrap server on the configured address.
+func (s *server) listenAndServe() error {
+	bl := &blob.Mux{"s3": s3blob.New(s.sess)}
 
 	http.Handle("/v1/execimage", rest.DoFuncHandler(newExecImageNode(bl), nil))
+	http.Handle("/v1/status", rest.DoFuncHandler(
+		func(ctx context.Context, call *rest.Call) {
+			if !call.Allow("GET") {
+				return
+			}
+			call.Reply(http.StatusOK, struct{}{})
+		}, nil))
 
-	server := &http.Server{Addr: s.Addr}
-	if s.Insecure {
+	const expiry = 10 * time.Minute
+	log.Printf("bootstrap server running, waiting (%s) for image...", expiry)
+	time.AfterFunc(expiry, func() {
+		log.Fatalf("no bootstrap image installed after %s; shutting down", expiry)
+	})
+
+	server := &http.Server{Addr: s.addr}
+	if s.insecure {
 		return server.ListenAndServe()
 	}
-	server.TLSConfig = s.ServerConfig
+	server.TLSConfig = s.config
 
 	return server.ListenAndServeTLS("", "")
 }
@@ -83,14 +156,5 @@ func newExecImageNode(m *blob.Mux) rest.DoFunc {
 		}
 
 		panic("should never reach")
-	}
-}
-
-func newStatusNode() rest.DoFunc {
-	return func(ctx context.Context, call *rest.Call) {
-		if !call.Allow("GET") {
-			return
-		}
-		call.Reply(http.StatusOK, struct{}{})
 	}
 }
--- cmd/reflowbootstrap/main.go
+++ cmd/reflowbootstrap/main.go
@@ -5,21 +5,10 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
-	"io/ioutil"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/grailbio/infra"
-	_ "github.com/grailbio/infra/aws"
-	_ "github.com/grailbio/infra/ec2metadata"
-	infratls "github.com/grailbio/infra/tls"
 	"github.com/grailbio/reflow/bootstrap"
-	infra2 "github.com/grailbio/reflow/infra"
-	"github.com/grailbio/reflow/log"
-	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -30,62 +19,5 @@ var (
 
 func main() {
 	flag.Parse()
-
-	schema := infra.Schema{
-		infra2.AWSCreds: new(credentials.Credentials),
-		infra2.Log:      new(log.Logger),
-		infra2.Session:  new(session.Session),
-		infra2.SSHKey:   new(infra2.SshKey),
-		infra2.TLS:      new(infratls.Authority),
-	}
-	schemaKeys := infra.Keys{
-		infra2.AWSCreds: "awscreds",
-		infra2.Log:      "logger",
-		infra2.Session:  "awssession",
-		infra2.SSHKey:   "key",
-		infra2.TLS:      "tls,file=/tmp/ca.reflow",
-	}
-
-	configFlag := *configFile
-	if configFlag != "" {
-		b, err := ioutil.ReadFile(configFlag)
-		if err != nil {
-			log.Fatal(err)
-		}
-		keys := make(infra.Keys)
-		if err := yaml.Unmarshal(b, keys); err != nil {
-			log.Fatal("config %v: %v", configFlag, err)
-		}
-		for k, v := range keys {
-			schemaKeys[k] = v
-		}
-	}
-	var err error
-	config, err := schema.Make(schemaKeys)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var sess *session.Session
-	if err = config.Instance(&sess); err != nil {
-		log.Fatal(err)
-	}
-	var tlsa *infratls.Authority
-	if err = config.Instance(&tlsa); err != nil {
-		log.Fatal(err)
-	}
-	_, serverConfig, err := tlsa.HTTPS()
-	if err != nil {
-		log.Fatal(err)
-	}
-	serverConfig.ClientAuth = tls.RequireAndVerifyClientCert
-
-	serv := bootstrap.BootstrapServer{
-		Addr:         *addr,
-		Insecure:     *insecure,
-		AwsSession:   sess,
-		ServerConfig: serverConfig,
-	}
-
-	log.Fatal(serv.ListenAndServe())
+	bootstrap.RunServer(bootstrap.DefaultSchema, bootstrap.DefaultSchemaKeys, *configFile, *addr, *insecure)
 }
