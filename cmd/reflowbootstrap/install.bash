a2658faf8f301a4e80bc4c9bcc7752fcb234c0bf
Swami Mahadevan <smahadevan@grailbio.com>
2019-10-10 13:56:14 -0700

    reflow/bootstrap: Add release scripts for Grail/OSS reflow bootstrap binaries
    Summary:
    Add scripts that build the reflow bootstrap binary (Grail and OSS versions)
    and push them to a public S3 bucket.
    
    In a subsequent revision, we will change reflow to fetch the bootstrap binary
    from these S3 locations to bootstrap the reflowlet running on an EC2 instance.
    
    Test Plan: Will be tested in a subsequent revision which uses these paths.
    
    Reviewers: marius, bbentson, jcharumilind, pgopal
    
    Reviewed By: bbentson
    
    Maniphest Tasks: T27890
    
    Differential Revision: https://phabricator.grailbio.com/D37547
    
    fbshipit-source-id: b42b680

---
+++ cmd/reflowbootstrap/install.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# install.bash is a script to build a reflow bootstrap binary and push to a public S3 bucket.
+# To build and release a new version, update VERSION below and run the script.
+# Make sure to update the latest path in: go/src/github.com/grailbio/reflow/cmd/reflow/main.go
+#
+# Reflow uses this binary as a bootstrap on reflowlet instances.
+#
+set -e
+set -x
+
+VERSION=reflowbootstrap0.1
+
+GOOS=linux GOARCH=amd64 go build -o /tmp/$VERSION .
+cloudkey ti-apps/admin aws s3 cp --acl public-read /tmp/$VERSION s3://grail-public-bin/linux/amd64/$VERSION
+
