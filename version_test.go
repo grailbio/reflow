2ae19bff4035631c0d100a01f0aa7ba75b851edc
Swami Mahadevan <smahadevan@grailbio.com>
2020-09-16 22:23:36 -0700

    reflow: Add more utility methods for version comparison
    Summary: Add more utility methods for version comparison
    
    Test Plan: Unit tests
    
    Reviewers: pboyapalli, pgopal
    
    Reviewed By: pboyapalli, pgopal
    
    Subscribers: pboyapalli, pgopal, dnicolaou
    
    Maniphest Tasks: T41260
    
    Differential Revision: https://phabricator.grailbio.com/D49312
    
    fbshipit-source-id: 1eaea6a

--- version_test.go
+++ version_test.go
@@ -20,6 +20,8 @@ func TestIsReflowSemVer(t *testing.T) {
 		{"1.1", true},
 		{"reflow1.1.1", true},
 		{"reflow1.1.1-beta", true},
+		{"reflowv1.1.1", true},
+		{"v1.1.1", true},
 	} {
 		if got, want := reflow.IsReflowSemVer(tc.v), tc.want; got != want {
 			t.Errorf("IsReflowSemVer(%s): got %v, want %v", tc.v, got, want)
@@ -27,17 +29,46 @@ func TestIsReflowSemVer(t *testing.T) {
 	}
 }
 
-func TestVersion(t *testing.T) {
+func TestIsOlderVersion(t *testing.T) {
 	for _, tc := range []struct {
 		v1, v2 string
-		want   bool
+		want   reflow.CompareResult
 	}{
-		{"reflow1.1.1", "reflow1.2.1", true},
-		{"reflow1.124.1", "1.2.1", false},
-		{"1.1.1", "reflow1.1.1", false},
+		{"reflow1.1.1", "reflow1.2.1", reflow.Lesser},
+		{"reflow1.124.1", "1.2.1", reflow.Greater},
+		{"1.1.1", "reflow1.1.1", reflow.Equal},
+		{"1.1.1", "1", reflow.Greater},
 	} {
-		if got, want := reflow.IsOlderVersion(tc.v1, tc.v2), tc.want; got != want {
+		if got, want := reflow.CompareVersions(tc.v1, tc.v2), tc.want; got != want {
 			t.Errorf("IsOlderVersion(%s, %s): got %v, want %v", tc.v1, tc.v2, got, want)
+		}
+	}
+}
+
+func TestIsAnySameOrNewer(t *testing.T) {
+	for _, tc := range []struct {
+		v    string
+		vs   []string
+		want bool
+	}{
+		{"reflow1.1.1", nil, false},
+		{"reflow1.1.1", []string{}, false},
+		{"reflow1.1.1", []string{"hello"}, false},
+		{"reflow1.1.1", []string{"reflow1.2.1"}, true},
+		{"reflow1.2.0", []string{"reflow1.1.1", "reflow1.1.3", "reflow1.2.1"}, true},
+		{"reflow1.1.1", []string{"reflow1.1.1", "reflow"}, true},
+		{"reflow1.1.1", []string{"1.2.1"}, true},
+		{"reflow1.2.0", []string{"1.1.1", "v1.1.3", "1.2.1"}, true},
+		{"reflow1.1.1", []string{"1.1.1", "reflow"}, true},
+		{"1.1.1", nil, false},
+		{"1.1.1", []string{}, false},
+		{"1.1.1", []string{"hello"}, false},
+		{"1.1.1", []string{"reflow1.2.1"}, true},
+		{"1.2.0", []string{"reflow1.1.1", "reflow1.1.3", "reflow1.2.1"}, true},
+		{"1.1.1", []string{"reflow1.1.1", "reflow"}, true},
+	} {
+		if got, want := reflow.IsAnySameOrNewerVersion(tc.v, tc.vs), tc.want; got != want {
+			t.Errorf("IsAnyOlderVersion(%s, %s): got %v, want %v", tc.v, tc.vs, got, want)
 		}
 	}
 }
