grailbio/reflow

commit bb2cbbc033fc32327624ba167d9257911936afce

reflow/eval: Add marshal limiter in some missing places
Summary: Add limiters in some missing places where we marshal large objects.

Test Plan: Integration tests.

Reviewers: pboyapalli

Reviewed By: pboyapalli

Subscribers: spolakh, dnicolaou

Tags: #reflow

Maniphest Tasks: T44121

Differential Revision: https://phabricator.grailbio.com/D51781

fbshipit-source-id: 819434f

flow/eval.go
@@ -1473,9 +1473,7 @@ func (e *Eval) CacheWrite(ctx context.Context, f *Flow, repo reflow.Repository) 
 	if err := e.Transferer.Transfer(ctx, e.Repository, repo, fs.Files()...); err != nil {
 		return err
 	}
-	_ = e.marshalLimiter.Acquire(ctx, 1)
-	id, err := marshal(ctx, e.Repository, &fs)
-	e.marshalLimiter.Release(1)
+	id, err := marshal(ctx, e.marshalLimiter, e.Repository, fs)
 	if err != nil {
 		return err
 	}
@@ -1512,7 +1510,7 @@ func (e *Eval) taskdbWrite(ctx context.Context, op Op, inspect reflow.ExecInspec
 		pid            digest.Digest
 	)
 	g, ctx := errgroup.WithContext(ctx)
-	if pid, err = marshal(ctx, e.Repository, inspect); err != nil {
+	if pid, err = marshal(ctx, e.marshalLimiter, e.Repository, inspect); err != nil {
 		log.Errorf("repository put profile: %v", err)
 	}
 	if exec != nil {
@@ -1630,9 +1628,7 @@ func (e *Eval) batchLookup(ctx context.Context, flows ...*Flow) {
 					}
 					continue
 				}
-				_ = e.marshalLimiter.Acquire(ctx, 1)
-				err = unmarshal(ctx, e.Repository, res.Digest, &fs)
-				e.marshalLimiter.Release(1)
+				err = unmarshal(ctx, e.marshalLimiter, e.Repository, res.Digest, &fs)
 				if err == nil {
 					e.Log.Debugf("cache.Lookup flow: %s (%s) result from key: %s, value: %s\n", f.Digest().Short(), f.Ident, key.Short(), res.Digest)
 					fsid = res.Digest
@@ -2689,13 +2685,17 @@ func printFileset(w io.Writer, prefix string, fs reflow.Fileset) {
 // Marshal marshals the value v and stores it in the provided
 // repository. The digest of the contents of the marshaled content is
 // returned.
-func marshal(ctx context.Context, repo reflow.Repository, v interface{}) (digest.Digest, error) {
+func marshal(ctx context.Context, l *limiter.Limiter, repo reflow.Repository, v interface{}) (digest.Digest, error) {
+	_ = l.Acquire(ctx, 1)
+	defer l.Release(1)
 	return repository.Marshal(ctx, repo, v)
 }
 
 // Unmarshal unmarshals the value named by digest k into v.
 // If the value does not exist in repository, an error is returned.
-func unmarshal(ctx context.Context, repo reflow.Repository, k digest.Digest, v interface{}) error {
+func unmarshal(ctx context.Context, l *limiter.Limiter, repo reflow.Repository, k digest.Digest, v interface{}) error {
+	_ = l.Acquire(ctx, 1)
+	defer l.Release(1)
 	return repository.Unmarshal(ctx, repo, k, v)
 }
 

flow/repair.go
@@ -7,6 +7,7 @@ package flow
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"sort"
 	"sync/atomic"
 	"time"
@@ -40,6 +41,8 @@ type Repair struct {
 	// NumWrites is incremented for each new assoc entry written by the repair job.
 	NumWrites int64
 
+	marshalLimiter *limiter.Limiter
+
 	writebacks chan writeback
 	g          *errgroup.Group
 }
@@ -50,12 +53,15 @@ type Repair struct {
 // (*Repair.Do).
 func NewRepair(config EvalConfig) *Repair {
 	r := &Repair{
-		EvalConfig: config,
-		writebacks: make(chan writeback, 1024),
+		EvalConfig:     config,
+		marshalLimiter: limiter.New(),
+		writebacks:     make(chan writeback, 1024),
 	}
 	if r.CacheLookupTimeout == time.Duration(0) {
 		r.CacheLookupTimeout = defaultCacheLookupTimeout
 	}
+	// Limit the number of concurrent marshal/unmarshal to the number of CPUs we have.
+	r.marshalLimiter.Release(runtime.NumCPU())
 	return r
 }
 
@@ -98,7 +104,7 @@ func (r *Repair) Do(ctx context.Context, f *Flow) {
 			}
 			continue
 		}
-		err = unmarshal(ctx, r.Repository, fsid, &fs)
+		err = unmarshal(ctx, r.marshalLimiter, r.Repository, fsid, &fs)
 		if err == nil {
 			hit = true
 			break

commit 5e1cfc02460fda955c65dac9c801149dc8485ce5

Fix a small bug that got introduced in D51781.
Summary:
Since `repository.Marshal` looks for a `Fileset` pointer to use
the custom marshaller, make sure to pass a pointer.

Reviewers: pboyapalli

Reviewed By: pboyapalli

Subscribers: dnicolaou

Maniphest Tasks: T44121

Differential Revision: https://phabricator.grailbio.com/D52351

fbshipit-source-id: 69b0ae6

flow/eval.go
@@ -1473,7 +1473,7 @@ func (e *Eval) CacheWrite(ctx context.Context, f *Flow, repo reflow.Repository) 
 	if err := e.Transferer.Transfer(ctx, e.Repository, repo, fs.Files()...); err != nil {
 		return err
 	}
-	id, err := marshal(ctx, e.marshalLimiter, e.Repository, fs)
+	id, err := marshal(ctx, e.marshalLimiter, e.Repository, &fs)
 	if err != nil {
 		return err
 	}