58b8e4555f088fa946665d53a3c724fa8cf751ee
Swami Mahadevan <smahadevan@grailbio.com>
2021-02-28 13:55:24 +0530

    reflow: Add a string/digest combo type
    Summary:
    Adding a convenience type `StringDigest` which holds any string
    and its digest together.  This type will be used in subsequent
    revisions whose primary purpose is to enable cost computation
    in Reflow.
    
    Test Plan: Trivial code.
    
    Reviewers: pboyapalli, ghorrell
    
    Reviewed By: ghorrell
    
    Subscribers: pbohannon
    
    Tags: #reflow
    
    Maniphest Tasks: T37166
    
    Differential Revision: https://phabricator.grailbio.com/D55460
    
    fbshipit-source-id: f1cf798

--- digester.go
+++ digester.go
@@ -12,3 +12,32 @@ import (
 )
 
 var Digester = digest.Digester(crypto.SHA256)
+
+// StringDigest holds any string and its digest.
+type StringDigest struct {
+	s string
+	d digest.Digest
+}
+
+// NewStringDigest creates a StringDigest based on the given string.
+func NewStringDigest(s string) StringDigest {
+	return StringDigest{
+		s: s,
+		d: Digester.FromString(s),
+	}
+}
+
+// String returns the underlying string.
+func (i StringDigest) String() string {
+	return i.s
+}
+
+// Digest returns the digest of the underlying string.
+func (i StringDigest) Digest() digest.Digest {
+	return i.d
+}
+
+// IsValid returns whether this StringDigest is valid (ie, the underlying string is non-empty).
+func (i StringDigest) IsValid() bool {
+	return i.s != ""
+}
