df53b4c88e983f1ff162fca4344eaeed2df6ec41
Swami Mahadevan <smahadevan@grailbio.com>
2021-09-11 00:02:52 -0700

    [SYSINFRA-683] reflow/tool: Make pred tool fetch profiles concurrently
    Summary: This change makes the `pred` tool fetch profiles of arguments concurrently.
    
    Test Plan: See [[ https://jira.ti-apps.aws.grail.com/browse/SYSINFRA-689 | SYSINFRA-689 ]].
    
    Reviewers: pboyapalli
    
    Reviewed By: pboyapalli
    
    Subscribers: dnicolaou
    
    Differential Revision: https://phabricator.grailbio.com/D64658
    
    fbshipit-source-id: 6ef4e47

--- tool/pred.go
+++ tool/pred.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/grailbio/base/data"
 	"github.com/grailbio/base/digest"
+	"github.com/grailbio/base/traverse"
 	"github.com/grailbio/reflow"
 	"github.com/grailbio/reflow/predictor"
 	"github.com/grailbio/reflow/taskdb"
@@ -24,6 +25,11 @@ var predCols = []headerDesc{
 	{"var", "variance of the statistic across the samples"},
 	{"first", "first time at which the statistic was profiled"},
 	{"last", "last time at which the statistic was profiled"},
+}
+
+type profResults struct {
+	arg   string
+	profs []reflow.Profile
 }
 
 func (c *Cmd) pred(ctx context.Context, args ...string) {
@@ -65,6 +71,30 @@ Valid values for -name are "mem" and "duration".
 		mi = *maxInspectFlag
 	}
 	p := predictor.New(repo, tdb, c.Log.Tee(nil, "predictor: "), cfg.MinData, mi, cfg.MemPercentile)
+	results := make(chan profResults)
+	go func() {
+		_ = traverse.Each(len(flags.Args()), func(i int) error {
+			arg := flags.Args()[i]
+			var q predictor.ProfileQuery
+			if d, err := digest.Parse(arg); err == nil {
+				q.ImgCmdId = taskdb.ImgCmdID(d)
+			} else {
+				q.Ident = arg
+			}
+			profs, err := p.QueryProfiles(ctx, q)
+			switch {
+			case err != nil:
+				c.Log.Errorf("invalid param %s: %v", arg, err)
+			case len(profs) == 0:
+				c.Log.Errorf("no data for param %s", arg)
+			default:
+				results <- profResults{arg, profs}
+			}
+			return nil
+		})
+		close(results)
+	}()
+
 	var (
 		tw tabwriter.Writer
 		w  io.Writer
@@ -72,22 +102,9 @@ Valid values for -name are "mem" and "duration".
 	tw.Init(c.Stdout, 4, 4, 1, ' ', 0)
 	defer func() { _ = tw.Flush() }()
 	w = &tw
-	for _, arg := range flags.Args() {
-		var q predictor.ProfileQuery
-		if d, err := digest.Parse(arg); err == nil {
-			q.ImgCmdId = taskdb.ImgCmdID(d)
-		} else {
-			q.Ident = arg
-		}
-		profs, err := p.QueryProfiles(ctx, q)
-		switch {
-		case err != nil:
-			c.Log.Errorf("invalid param %s: %v", arg, err)
-			continue
-		case len(profs) == 0:
-			c.Log.Errorf("no data for param %s", arg)
-			continue
-		}
+
+	for r := range results {
+		arg, profs := r.arg, r.profs
 		distKeys := map[string]bool{}
 		for _, p := range profs {
 			for k, v := range p {
