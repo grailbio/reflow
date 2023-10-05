8075b25b3d1381be5f939465edb7204d1269b4cc
Pedro Fialho <pfialho@grailbio.com>
2023-10-05 14:29:40 +0000

    [SYSINFRA-3316] update flatcar and instance types (grailbio/grail!20671)
    Approved-by: Dave Shepard <dshepard@grailbio.com>
    
    GitLab URL: https://gitlab.com/grailbio/grail/-/merge_requests/20671
    
    fbshipit-source-id: 15c50fe

--- ec2cluster/amis.go
+++ ec2cluster/amis.go
@@ -2,10 +2,10 @@
 // Generated from URL: https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_ami_all.json
 // FLATCAR_BUILD=3510
 // FLATCAR_BRANCH=2
-// FLATCAR_PATCH=6
-// FLATCAR_VERSION=3510.2.6
-// FLATCAR_VERSION_ID=3510.2.6
-// FLATCAR_BUILD_ID="2023-08-07-1626"
+// FLATCAR_PATCH=8
+// FLATCAR_VERSION=3510.2.8
+// FLATCAR_VERSION_ID=3510.2.8
+// FLATCAR_BUILD_ID="2023-09-19-1914"
 // FLATCAR_SDK_VERSION=3510.0.0
 
 package ec2cluster
@@ -17,27 +17,27 @@ import (
 )
 
 var flatcarAmiByRegion = map[string]string{
-	"af-south-1":     "ami-0f65cba92b090d9bd",
-	"ap-east-1":      "ami-05ae6f5ed5e180227",
-	"ap-northeast-1": "ami-098fdc56ca0e74c4e",
-	"ap-northeast-2": "ami-0059a10861a323893",
-	"ap-south-1":     "ami-0129ec8fa007ccbc9",
-	"ap-southeast-1": "ami-030e74a8aa78cea2a",
-	"ap-southeast-2": "ami-0e870aed71e91b6aa",
-	"ap-southeast-3": "ami-021ee199ab285a2d8",
-	"ca-central-1":   "ami-05078bc9a3e5a87fc",
-	"eu-central-1":   "ami-01e8d5553e3d74da1",
-	"eu-north-1":     "ami-0caa27870781d0253",
-	"eu-south-1":     "ami-07fe82f740deec21b",
-	"eu-west-1":      "ami-0eb5c17b8ff82a90b",
-	"eu-west-2":      "ami-0fa82a03ecfd4f048",
-	"eu-west-3":      "ami-0bd47dc70046f0dd8",
-	"me-south-1":     "ami-01ca41a5c57a95869",
-	"sa-east-1":      "ami-077c11aaefc590fe2",
-	"us-east-1":      "ami-0f22961936c6c0405",
-	"us-east-2":      "ami-0aa7c49092f6ff964",
-	"us-west-1":      "ami-0973cb1190384f029",
-	"us-west-2":      "ami-0af1032ee364853f0",
+	"af-south-1":     "ami-018d9b9d7151ab6c8",
+	"ap-east-1":      "ami-0a20232670e0772af",
+	"ap-northeast-1": "ami-0233879a9dd92df11",
+	"ap-northeast-2": "ami-01bff27d84a7a7700",
+	"ap-south-1":     "ami-0cc2acda59cd3af78",
+	"ap-southeast-1": "ami-0c42cc418ecd92604",
+	"ap-southeast-2": "ami-01691d179b95f6f8c",
+	"ap-southeast-3": "ami-0dfb599c8fc910601",
+	"ca-central-1":   "ami-09dc356370f64513d",
+	"eu-central-1":   "ami-068829f5bf9333501",
+	"eu-north-1":     "ami-0a560c9c18e52fe3a",
+	"eu-south-1":     "ami-0b069d1752bac2e31",
+	"eu-west-1":      "ami-0e28f7527ce1dd0d1",
+	"eu-west-2":      "ami-03f8f46c0d8a86c5c",
+	"eu-west-3":      "ami-0328a3887119e6dd3",
+	"me-south-1":     "ami-0f34884e3fbf09f49",
+	"sa-east-1":      "ami-0151f43dfd95a3865",
+	"us-east-1":      "ami-0604fd4b5a31a9e9f",
+	"us-east-2":      "ami-0a804cb708f395c8a",
+	"us-west-1":      "ami-0ba006ff9272db530",
+	"us-west-2":      "ami-0c3488073a14ab0af",
 }
 
 // GetAMI gets the AMI ID for the AWS region derived from the given AWS session.
